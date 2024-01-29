14f714accea391685bd0243d016e3e6bea2cf07a
Guilherme Diniz <guilhermedinizlau@gmail.com>
2024-01-29 08:55:12 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/guilherme/course/entities/OrderItem.java
+++ src/main/java/com/guilherme/course/entities/OrderItem.java
@@ -38,7 +38,7 @@ public class OrderItem implements Serializable {
         id.setOrder(order);
     }
 
-    public Product geProduct() {
+    public Product getProduct() {
         return id.getProduct();
     }
 
